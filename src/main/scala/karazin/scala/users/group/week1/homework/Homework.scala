0fdf07bc5281fd6c19a18ee2f832c54cf2da0cba
Locomody <67393547+Locomody@users.noreply.github.com>
2021-11-25 08:58:27 +0200

    Week-1 - First week solutions

--- src/main/scala/karazin/scala/users/group/week1/homework/Homework.scala
+++ src/main/scala/karazin/scala/users/group/week1/homework/Homework.scala
@@ -40,26 +40,135 @@ object Homework :
 
   object `Boolean Operators` :
 
-    def not(b: Boolean): Boolean = ???
+    def not(b: Boolean): Boolean = if b then false else true
 
-    def and(left: Boolean, right: Boolean): Boolean = ???
+    def and(left: Boolean, right: Boolean): Boolean =
+                            // it's another variant to do this,
+                            // but I prefer more explicitness and readability
 
-    def or(left: Boolean, right: Boolean): Boolean = ???
+      if left then          // if left then
+        if right then true  //    if right then right
+        else right          //    else right
+      else left            // else left
+
+    def or(left: Boolean, right: Boolean): Boolean = if left then true else right
 
   end `Boolean Operators`
 
   object `Fermat Numbers` :
 
-    val multiplication: (BigInt, BigInt) => BigInt = ???
+    val multiplication: (BigInt, BigInt) => BigInt = (left, right) =>
+    {
 
-    val power: (BigInt, BigInt) => BigInt = ???
+      @tailrec
+      def multiplicationReq(left: BigInt, right: BigInt, acc :BigInt): BigInt =
+        if right == 0 then acc
+        else multiplicationReq(left, right - 1, acc + left)
 
-    val fermatNumber: Int => BigInt = ???
+
+      multiplicationReq(left, right, acc = 0)
+    }
+
+    val power: (BigInt, BigInt) => BigInt = (left, right) =>
+    {
+
+      @tailrec
+      def powerReq(left: BigInt, right: BigInt, acc: BigInt): BigInt =
+        if right == 0 then acc
+        else powerReq(left, right - 1, multiplication(acc, left))
+
+      powerReq(left, right, acc = 1)
+    }
+
+    val fermatNumber: Int => BigInt = (n:Int) => power(2, power(2, n)) + 1
 
   end `Fermat Numbers`
 
   object `Look-and-say Sequence` :
-    val lookAndSaySequenceElement: Int => BigInt = ???
+    // Why scala String != List[Char], so sad :-(
+
+    val length: (List[Char]) => Int = (str) =>
+    {
+      @tailrec
+      def lenghtRec(str: List[Char], acc: Int): Int =
+        str match
+        {
+          case Nil => acc
+          case _ :: tail => lenghtRec(tail, acc + 1)
+        }
+
+      lenghtRec(str, acc = 0)
+    }
+
+    val stringHead: (List[Char]) => Char = (list) =>
+    {
+      list match
+      {
+        case Nil => '\u0000'
+        case head::_ => head
+      }
+    }
+
+    val isStringEmpty: (List[Char]) => Boolean = (list) =>
+    {
+      list == Nil
+    }
+
+    // It's not optimized because of Strings immutability.
+    // The more effective way to use StringBuilder maybe (???)
+    val CharListToString: (List[Char]) => String = (list) =>
+    {
+      @tailrec
+      def CharListToStringRec(list: List[Char], str: String): String =
+        list match
+        {
+          case Nil => str
+          case head :: tail => CharListToStringRec(tail, str ++ head.toString)
+        }
+
+      CharListToStringRec(list, str = "")
+    }
+
+    val splitStringIntoClusters: (List[Char]) => List[List[Char]] = (str) =>
+    {
+      @tailrec
+      def splitStringIntoClustersRec(str: List[Char], clusters: List[List[Char]],
+                                     cluster: List[Char]): List[List[Char]] =
+        str match
+        {
+          case Nil => clusters :+ cluster
+          case head :: tail =>
+            (if (!isStringEmpty(tail)) && (head != stringHead(tail))
+            then splitStringIntoClustersRec(tail, clusters :+ (cluster :+ head), List())
+            else splitStringIntoClustersRec(tail, clusters, cluster :+ head))
+        }
+
+      splitStringIntoClustersRec(str, List(), List())
+    }
+
+    val lookAndSayNext: (List[Char]) => List[Char] = (str) =>
+    {
+      @tailrec
+      def lookAndSayNextRec(sequence: List[List[Char]], nextSequence: List[Char]): List[Char] =
+        sequence match
+        {
+          case Nil => nextSequence
+          case head :: tail => lookAndSayNextRec(tail, nextSequence :+ (length(head) + 48).toChar :+ stringHead(head))
+        }
+
+      lookAndSayNextRec(splitStringIntoClusters(str), List())
+    }
+
+    val lookAndSaySequenceElement: Int => BigInt = (n: Int) =>
+    {
+      @tailrec
+      def lookAndSaySequenceElementRec(element: List[Char], n: Int): List[Char] =
+        if n == 0
+        then element
+        else lookAndSaySequenceElementRec(lookAndSayNext(element), n - 1)
+
+      BigInt(CharListToString(lookAndSaySequenceElementRec(List('1'), n)))
+    }
 
   end `Look-and-say Sequence`
 
--- src/test/scala/karazin/scala/users/group/week1/homework/HomeworkSpecification.scala
+++ src/test/scala/karazin/scala/users/group/week1/homework/HomeworkSpecification.scala
@@ -4,12 +4,13 @@ import org.scalacheck._
 import Prop.{forAll, propBoolean}
 import Homework._
 import karazin.scala.users.group.week1.homework.arbitraries
+import scala.annotation.tailrec
 
 object HomeworkSpecification extends Properties("Homework"):
 
   include(BooleanOperatorsSpecification)
   include(FermatNumbersSpecification)
-  include(LookAndAaSequenceSpecification)
+  include(LookAndSaySequenceSpecification)
 
 end HomeworkSpecification
 
@@ -23,13 +24,13 @@ object BooleanOperatorsSpecification extends Properties("Boolean Operators"):
   property("and") = forAll { (pair: (Boolean, Boolean)) =>
     val (left, right) = pair
     
-    and(left, right) == left && right
+    and(left, right) == (left && right)
   }
 
   property("or") = forAll { (pair: (Boolean, Boolean)) =>
     val (left, right) = pair
     
-    or(left, right) == left || right
+    or(left, right) == (left || right)
   }   
 
 end BooleanOperatorsSpecification
@@ -46,18 +47,34 @@ object FermatNumbersSpecification extends Properties("Fermat Numbers"):
     power(left, right) == (0 until right).foldLeft(BigInt(1)) { (acc, _) => acc * left }
   }
 
+ // Math.pow(2, Math.pow(2, Math.abs(n))) + 1 returns
+ // differ value type from BigInt than specified in FermatNumber. So i change right value
   property("fermatNumber") = forAll { (n: Int) =>
-    fermatNumber(n) == Math.pow(2, Math.pow(2, 2)) + 1
+    fermatNumber(n) == BigInt(2).pow(Math.pow(2, n).intValue()) + 1
   }  
 
 end FermatNumbersSpecification
 
-object LookAndAaSequenceSpecification extends Properties("Look-and-say Sequence"):
+object LookAndSaySequenceSpecification extends Properties("Look-and-say Sequence"):
   import `Look-and-say Sequence`._
   import arbitraries.given Arbitrary[Int]
 
-  property("fermatNumber") = forAll { (n: Int) =>
-    lookAndSaySequenceElement(n) == 42
+  val modelLookAndSaySequence: (Int) => BigInt = (n: Int) => {
+    val regex = """(\d)(\1*)""".r
+    def lookAndSay(idx: Int, acc: String): String = {
+      if (idx == n) acc
+      else lookAndSay(
+        idx + 1,
+        regex
+          .findAllMatchIn(acc)
+          .map(m => m.group(0).length.toString + m.group(1)).mkString)
+    }
+
+    BigInt(lookAndSay(0, "1"))
+  }
+
+  property("LookAndSaySequence") = forAll { (n: Int) =>
+    lookAndSaySequenceElement(n) == modelLookAndSaySequence(n)
   }  
 
-end LookAndAaSequenceSpecification
+end LookAndSaySequenceSpecification
